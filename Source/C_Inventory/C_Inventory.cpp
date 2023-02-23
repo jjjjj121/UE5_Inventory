0fb3269ac90e6201ab905e24933c8c78b23fa3eb
jjjjj121 <104483496+jjjjj121@users.noreply.github.com>
2023-02-23 15:58:02 +0900

    init

---
+++ Source/C_Inventory/C_Inventory.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "C_Inventory.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, C_Inventory, "C_Inventory" );
+ 
